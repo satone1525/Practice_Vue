30f58acfff8f0fe79d8192a175b22bf3417827e6
satone <kyotosato1107@gmail.com>
2025-07-22 12:44:41 +0900

    動画5本目終了（v-ifとv-else）

--- vue_practice/src/App.vue
+++ vue_practice/src/App.vue
@@ -1,13 +1,29 @@
 <script setup>
 import { ref } from 'vue'
-const number = ref(0)
-const add = () => {
-  number.value++
+
+const todos = ref([])
+const nweTodo = ref('')
+
+const addTodo = () => {
+  todos.value.push(nweTodo.value)
+  nweTodo.value = ''
 }
+
+const removeTodo = (index) => {
+  todos.value.splice(index, 1)    //要素から1つ消す
+}
+
 </script>
+
 <template>
-<input type="text" size="30" v-model="number">
-<button @click="add()"> +1 </button>  
+<input type="text" size="30" v-model="nweTodo">
+<button @click="addTodo()"> 追加 </button>  
+
+<ul v-if="todos.length > 0">
+  <li v-for="(todo, i) in todos" v-bind:key="i"> {{ todo }} <span @click="removeTodo(i)" style="cursor:pointer"> x </span> </li>
+
+</ul>
+<p v-else>※ToDoを追加してください</p>
 </template>
 
 <!--　省略形　メモ 
@@ -28,5 +44,13 @@ const add = () => {
 // import { ref, computed } from 'vue'
 // const myname = ref('')
 // const message = computed(() => 'ようそこ！ <strong>' + myname.value + '</strong>さん')
-// ...
+-->
+
+<!-- script をコメントアウト 3つ目の動画終了時にはここがscript setupだった (ボタンクリックすると数が増える)  -->
+<!--
+// import { ref } from 'vue'
+// const number = ref(0)
+// const add = () => {
+//   number.value++
+// }
 -->
